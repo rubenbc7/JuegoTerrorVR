d21724cd8f79c01d6faa93483b2f283e4112d8c2
Alberto Hoyos <jesalbertohyfx@hotmail.com>
2022-11-11 21:12:38 -0700

    aparicion

---
+++ Assets/Scripts/Ataque.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ataque : MonoBehaviour
+{
+    public Vision vision;
+    [SerializeField] string animation_name;
+    private float _elapsedTime = 0.0f;
+    [SerializeField] float _anim_time;
+
+    [SerializeField] float speed;
+
+    [SerializeField] AudioClip sonido;
+    [SerializeField] AudioSource audioSource {get{return GetComponent<AudioSource>();}}
+
+    void Update()
+    {
+        Animation();
+        _elapsedTime += Time.deltaTime;
+        if(_elapsedTime >= _anim_time) {
+            Destroy(gameObject);
+        }
+    }
+    void Animation()
+    {
+        if(vision.canSeeEnemy)
+        {
+            gameObject.GetComponent<Animator>().Play(animation_name);
+            audioSource.PlayOneShot(sonido);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+
+    
+}
---
+++ Assets/Scripts/Vision.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vision : MonoBehaviour
+{
+    public float radius;
+    [Range(0,360)]
+    public float angle;
+
+    public GameObject enemyRef;
+
+    [SerializeField] LayerMask targetMask;
+    [SerializeField] LayerMask obstructionMask;
+
+    public bool canSeeEnemy;
+
+    private void Start()
+    {
+        enemyRef = GameObject.FindGameObjectWithTag("Enemy");
+        StartCoroutine(FOVRoutine());
+    }
+
+    private IEnumerator FOVRoutine()
+    {
+        WaitForSeconds wait = new WaitForSeconds(0.2f);
+
+        while (true)
+        {
+            yield return wait;
+            FieldOfViewCheck();
+        }
+    }
+
+    private void FieldOfViewCheck()
+    {
+        Collider[] rangeChecks = Physics.OverlapSphere(transform.position, radius, targetMask);
+
+        if (rangeChecks.Length != 0)
+        {
+            Transform target = rangeChecks[0].transform;
+            Vector3 directionToTarget = (target.position - transform.position).normalized;
+
+            if (Vector3.Angle(transform.forward, directionToTarget) < angle / 2)
+            {
+                float distanceToTarget = Vector3.Distance(transform.position, target.position);
+
+                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask))
+                    canSeeEnemy = true;
+                else
+                    canSeeEnemy = false;
+            }
+            else
+                canSeeEnemy = false;
+        }
+        else if (canSeeEnemy)
+            canSeeEnemy = false;
+    }
+}

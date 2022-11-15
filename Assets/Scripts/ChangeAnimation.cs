3b309d2256e58c575f13b320f70b857b0bc49c5d
Rubén Borbolla <rubenborbollacastelo@gmail.com>
2022-11-14 21:06:33 -0700

    Screamers y mona

---
+++ Assets/Scripts/ChangeAnimation.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimation : MonoBehaviour
+{
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>(); 
+    }
+
+    // Update is called once per frame
+    public void CambiarAnim()
+    {
+         anim.Play("anim2");
+    }
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/script.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script : MonoBehaviour
+{
+    public GameObject screamer;
+    public float delay = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        screamer.GetComponent<SpriteRenderer> ().color = new Color(1, 1, 1, 0);
+    }
+    public void PointerClick()
+    {
+        //screamer.SetActive(true);
+       
+       // StartCoroutine(FadeIn());
+        StartCoroutine(FadeImage(true));
+        
+    }
+   
+
+     IEnumerator FadeImage(bool fadeAway)
+    {
+        // fade from opaque to transparent
+        if (fadeAway)
+        {
+            // loop over 1 second backwards
+            for (float i = 1f; i >= 0; i -= Time.deltaTime)
+            {
+                // set color with i as alpha
+                screamer.GetComponent<SpriteRenderer> ().color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+        }
+        // fade from transparent to opaque
+        else
+        {
+            // loop over 1 second
+            for (float i = 0; i <= 1f; i += Time.deltaTime)
+            {
+                // set color with i as alpha
+                screamer.GetComponent<SpriteRenderer> ().color = new Color(1, 1, 1, i);
+                yield return null;
+            }
+            
+        }
+        
+    }
+}
